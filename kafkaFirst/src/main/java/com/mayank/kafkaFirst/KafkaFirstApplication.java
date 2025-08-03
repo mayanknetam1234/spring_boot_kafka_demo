8e1f12c3139e26dc836f8f9e372719a2c4e65de1
mayanknetam1234 <netammayank547@gmail.com>
2025-08-03 19:32:52 +0530

    first commit

---
+++ kafkaFirst/src/main/java/com/mayank/kafkaFirst/KafkaFirstApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.kafkaFirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaFirstApplication.class, args);
+	}
+
+}
---
+++ kafkaFirst/src/test/java/com/mayank/kafkaFirst/KafkaFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.kafkaFirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
